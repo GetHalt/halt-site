45b04dca7f56fbf68c606d6aaeb412ff90113362
Ram Ratan Maurya <ratan.maurya@gmail.com>
2014-03-02 17:50:21 +0530

    Ready!

--- footer.php
+++ footer.php
@@ -11,6 +11,18 @@
 	</div><!-- #content -->
 </div><!-- #page -->
 
+<footer class="site-footer" role="contentinfo">
+	<div class="inside">
+		<p>Copyright &copy; 2014 Halt. A project by <a href="//twitter.com/mauryaratan">Ram Ratan Maurya</a>.</p>
+
+		<p>
+			<a href="//twitter.com/GetHalt" class="twitter">Twitter</a> /
+			<a href="//facebook.com/GetHalt" class="facebook">Facebook</a> /
+			<a href="//github.com/GetHalt" class="Github">Github</a>
+		</p>
+	</div>
+</footer>
+
 <?php wp_footer(); ?>
 
 </body>
